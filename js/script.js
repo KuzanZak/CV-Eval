69c03c005b5726eb0204f34d4c09deeef2509d06
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-22 14:01:20 +0200

    create modal js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@ const link = document.getElementById("loisirs-link");
 
 link.addEventListener("click", function(event){
     event.preventDefault()
-
-    
-    console.log("click")
+    const modalLink = modal()
+    console.log(modalLink)
 })
+
+function modal(){
+    const modalLink = document.createElement("div");
+    modalLink.classList.add("modal-link")
+    modalLink.id = "modal-link";
+
+    const modalLinkContent = document.createElement("div");
+    modalLinkContent.classList.add("modal-link-content")
+    modalLink.appendChild(modalLinkContent);
+    document.getElementById("loisirs").insertBefore(modalLink, document.getElementById("loisirs-content"));
+    return modalLink
+};
