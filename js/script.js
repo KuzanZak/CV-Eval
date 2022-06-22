f01e5d00bc1e781f4d36ddd3d83dd56e8150a7f9
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-22 15:36:53 +0200

    modal desktop

--- js/script.js
+++ js/script.js
@@ -12,32 +12,46 @@ function modal(){
     modalLinkContent.classList.add("modal-link-content")
     modalLink.appendChild(modalLinkContent);
     document.getElementById("loisirs").insertBefore(modalLink, document.getElementById("loisirs-content"));
-    
     modalLink.addEventListener("click", function(event) {
         if (event.target.classList.contains("modal-link-close")) {
             remove()
             this.remove();
         }
     })
-    return modalLink
+    return modalLinkContent
 };
 
 function add() {
     linkLogoLakers.classList.add("display")
-    linkPLakers.classList.add("display")
+    linkPLakers.classList.add("displayF")
     linkDuoLakers.classList.add("display")
 }
 
 function remove(){
     linkLogoLakers.classList.remove("display")
-    linkPLakers.classList.remove("display")
+    linkPLakers.classList.remove("displayF")
     linkDuoLakers.classList.remove("display")
 
+}
+
+
+function icon(){
+    const iconClose = document.createElement("i");
+    iconClose.classList.add("fa")
+    iconClose.classList.add("fa-times-circle")
+    iconClose.classList.add("modal-link-close")
+    iconClose.setAttribute("aria-hidden", "true")
+    return iconClose
 }
 
 link.addEventListener("click", function(event){
     event.preventDefault();
     add();
-    const modalLink = modal();
-    modalLink.firstElementChild.innerHTML = '<i class="fa fa-times-circle modal-link-close" aria-hidden="true"></i>';
+    const modalLinkContent = modal();
+    const iconClose = icon();
+    modalLinkContent.appendChild(linkLogoLakers)
+    modalLinkContent.appendChild(linkPLakers)
+    modalLinkContent.appendChild(linkDuoLakers)
+    icon()
+    modalLinkContent.insertBefore(iconClose, linkLogoLakers)
 })
