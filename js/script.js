1bd6df7044a91f4ff6fb3f80ce52f207d55a8027
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-22 14:49:50 +0200

    modal finish

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 const link = document.getElementById("loisirs-link");
-
-
-link.addEventListener("click", function(event){
-    event.preventDefault()
-    const modalLink = modal()
-    modalLink.firstElementChild.innerHTML = '<i class="fa fa-times-circle modal-link-close" aria-hidden="true"></i>';
-})
+const linkLogoLakers = document.getElementById("modal-link-img");
+const linkDuoLakers = document.getElementById("modal-link-img2");
+const linkPLakers = document.getElementById("modal-link-p");
 
 function modal(){
     const modalLink = document.createElement("div");
@@ -18,8 +14,30 @@ function modal(){
     document.getElementById("loisirs").insertBefore(modalLink, document.getElementById("loisirs-content"));
     
     modalLink.addEventListener("click", function(event) {
-        if (event.target.classList.contains("modal-link-close")) this.remove();
-        console.log("click")
+        if (event.target.classList.contains("modal-link-close")) {
+            remove()
+            this.remove();
+        }
     })
     return modalLink
 };
+
+function add() {
+    linkLogoLakers.classList.add("display")
+    linkPLakers.classList.add("display")
+    linkDuoLakers.classList.add("display")
+}
+
+function remove(){
+    linkLogoLakers.classList.remove("display")
+    linkPLakers.classList.remove("display")
+    linkDuoLakers.classList.remove("display")
+
+}
+
+link.addEventListener("click", function(event){
+    event.preventDefault();
+    add();
+    const modalLink = modal();
+    modalLink.firstElementChild.innerHTML = '<i class="fa fa-times-circle modal-link-close" aria-hidden="true"></i>';
+})
